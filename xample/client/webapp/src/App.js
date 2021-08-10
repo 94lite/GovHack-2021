6d94316e95d0bef7ad8c792657428ff66e50c609
Lite W Kim <litek@orionhealth.com>
2021-08-10 19:25:13 +1200

    Update react app with button Connect with gunicorn/flask server

--- xample/client/webapp/src/App.js
+++ xample/client/webapp/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import DisplayTime from './DisplayTime';
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          <DisplayTime />
+        </div>
       </header>
     </div>
   );
---
+++ xample/client/webapp/src/DisplayTime.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export default function DisplayTime() {
+  const [time, setTime] = useState(undefined);
+
+  function getTime() {
+    axios
+      .get("/api/time")
+      .then(res => {
+        console.log(res);
+        if (res.data.success) {
+          setTime(res.data.time);
+        }
+      })
+      .catch(err => console.log(err));
+  }
+
+  return (
+    <div style={style}>
+      <button onClick={() => getTime()}>get time</button>
+      <div>{time}</div>
+    </div>
+  )
+}
+
+const style = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-evenly',
+  alignContent: 'center',
+  height: '100px'
+}
